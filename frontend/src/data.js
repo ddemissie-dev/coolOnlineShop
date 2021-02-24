79c579aef650f2d4deb27729699bc8c058d71d27
dereje <mssharepoint777@gmail.com>
2021-02-24 00:20:09 -0500

    My first Commit

---
+++ frontend/src/data.js
@@ -0,0 +1,159 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            name:'የኛ ስታይል',
+            category:'Dresses',
+            image:'./images/p1.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+        {
+            _id:'2',
+            name:'ሰመር ስታይል',
+            category:'Dresses',
+            image:'./images/P3.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'3',
+            name:'ሱዚ ስታይል',
+            category:'Dresses',
+            image:'./images/p2.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'4',
+            name:'ዊክኤንድ ስታይል',
+            category:'Shoes',
+            image:'./images/p4.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'5',
+            name:'አዲስ ስታይል',
+            category:'Dresses',
+            image:'./images/p5.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'6',
+            name:'ኩል ስታይል',
+            category:'Shirt',
+            image:'./images/p6.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'7',
+            name:'ሄጳ ስታይል',
+            category:'Shirt',
+            image:'./images/p7.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'8',
+            name:'ሃበሻዊት ስታይል',
+            category:'Bike',
+            image:'./images/p14.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'9',
+            name:'ዊንተር ስታይል',
+            category:'Shoes',
+            image:'./images/p17.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'10',
+            name:'ልጆች ስታይል',
+            category:'Dresses',
+            image:'./images/p11.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'11',
+            name:'ኢትዮጵያዊት ስታይል',
+            category:'Dresses',
+            image:'./images/p15.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'12',
+            name:'ወሎ ስታይል',
+            category:'Dresses',
+            image:'./images/p12.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        },
+
+        {
+            _id:'13',
+            name:'ቡና ስታይል',
+            category:'Dresses',
+            image:'./images/p13.jpg',
+            price:120,
+            brand:'Ethiopia',
+            rating:4.5,
+            numReviews:10,
+            description:'Ethiopian high quality product'  
+        }
+    ]
+}
+export default data
